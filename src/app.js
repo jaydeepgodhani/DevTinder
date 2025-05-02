887baff2380b5a2b070b9d80ba2454ebcc7fc971
Jaydeep Godhani <jaydeep@Hnvk.local>
2025-05-02 11:34:29 +0530

    setting cookie

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ const connectDb = require("./config/database");
 const app = express();
 const User = require("./models/user");
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
 
 // this is middleware provided by express by default to get json data
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
